3483201d1a1510a78dca6c2c4a34ddc6154ccf80
Rafael Companhoni <companhoni@gmail.com>
2017-03-27 20:01:30 -0300

    A bit more on the initial structure

--- performance prediction/doubling_ratio.js
+++ performance prediction/doubling_ratio.js
@@ -1,10 +1,39 @@
 /**
- * Proxy to manage the stopwatch at the beginning and end of the target algorithm execution 
+ * Proxy to manage the stopwatch at the beginning and end of the target algorithm execution
+ * http://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/DoublingRatio.java.html 
 */
 
 "use strict";
 
-module.exports = function () {
-   // TODO 
+var searching = require("searching.js");
+
+function timeTrial(n) {
+    var a = [];
+
+    for (let i = 0; i < n; i++) {
+        a[i] = Math.floor((Math.random() * Number.MAX_SAFE_INTEGER) + Number.MIN_SAFE_INTEGER);
+    }
+
+    console.time("timer");
+
+    // TODO -- call target' function' being measured
+    //ThreeSum.count(a);
+
+    console.timeEnd("timer");
+
+    return 0;    
 }
 
+// TODO - generate data 
+// TODO - run searching.linearSearch 
+// TODO - run searching.linearSearch
+var prev = timeTrial(125);
+
+for (let n = 250; true; n += n) {
+    let time = timeTrial(n);
+    
+    let results = util.format("%7d %7.1f %5.1f\n", n, time, time/prev);
+    console.log(results);
+
+    prev = time;
+} 
--- performance prediction/searching.js
+++ performance prediction/searching.js
@@ -1,5 +1,9 @@
 "use strict";
 
-module.exports = function () {
-   // TODO: linear and binary search
-}
+module.exports.linearSearch = function(array, value) {
+    return -1;
+};
+
+module.exports.binarySearch = function(input) {
+    return -1;
+};
