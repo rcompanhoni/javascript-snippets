b348ff55227c2b9212e15aec3c57250a84872e67
RCompanhoni <companhoni@gmail.com>
2017-05-29 01:25:44 -0300

    Blog form (partial)

--- redux blog app/src/components/posts_new.js
+++ redux blog app/src/components/posts_new.js
@@ -1,13 +1,79 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-    render() {
+    renderField(field) {
+        const { meta: {touched, error } } = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
         return (
-            <div>
-                Posts New!
+            <div className={className}>
+                <label>{field.label}</label>
+
+                <input 
+                    className="form-control"
+                    type="text"
+                    {...field.input} // binds the redux-form Field component with the HTML event handlers (e.g onChange, onFocus, onBlur, etc)
+                />
+
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
         )
+    }
+
+    onSubmit(values) {
+        console.log(values);
+    }
+
+    render() {
+        const { handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field 
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                 <Field 
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                 <Field 
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        );
     };
 }
 
-export default PostsNew;
+// called when the form is submitted
+function validate(values) {
+    const errors = {};
+
+    if (!values.title || values.title.length < 3) {
+        errors.title = "Enter a title that is at least 3 characters";
+    }
+
+    if (!values.categories) {
+        errors.categories = "Enter some categories";
+    }
+
+    if (!values.content) {
+        errors.content = "Enter some content, please";
+    }
+
+    return errors; // if this is empty then the form is fine to submit
+}
+
+export default reduxForm({ 
+    validate,
+    form: 'PostNewForm'    
+ })(PostsNew);
