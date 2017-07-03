13703ca1784de22defc89308eef2a6317e1d3b27
Rafael Companhoni <companhoni@gmail.com>
2017-07-03 18:09:00 -0300

    Prepares login/logout button structure (container, action creator and reducer)

--- react higher order components/src/components/app.js
+++ react higher order components/src/components/app.js
@@ -6,6 +6,7 @@ export default class App extends Component {
     return (
       <div>
         <Header />
+        {this.props.children} {/* needed for routing */}
       </div>
     );
   }
---
+++ react higher order components/src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            PROTECTED RESOURCES
+
+            <ul>
+                <li>ITEM 1</li>
+                <li>ITEM 2</li>
+                <li>ITEM 3</li>
+            </ul>
+        </div>
+    );
+};
