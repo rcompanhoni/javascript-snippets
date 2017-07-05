2b0a43d82b1ae24c26f92bcf0d72fa0a0fd4695d
Rafael Companhoni <companhoni@gmail.com>
2017-07-05 15:04:30 -0300

    bcrypt-nodejs

--- redux authentication/README.md
+++ redux authentication/README.md
@@ -3,3 +3,4 @@
 Tutorial from Udemy's 'Advanced React and Redux' by Stephen Grider. 
 
 * *Back end with Express*: Basic modules for an Express application (express, body-parser, morgan, nodemon)
+* *bcrypt-nodejs*: 
--- redux authentication/models/user.js
+++ redux authentication/models/user.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // define the model
 const userSchema = new Schema({
     email: { type: String, required: true, lowercase: true },
-    passport: String
+    password: String
+});
+
+// on save hook, encrypt password
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    // generate salt
+    bcrypt.genSalt(10, function(err, salt) {
+        if(err) { return next(err) };
+
+        // use salt to encrypt the password
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) { return next(err) }
+
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 // create the actual model class (corresponds to a Mongo collection named 'user')
