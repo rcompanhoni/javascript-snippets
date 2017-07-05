6b9f66e4426d441dd004d7478b96c55d25012e8c
Rafael Companhoni <companhoni@gmail.com>
2017-07-05 14:13:55 -0300

    POST /signup

---
+++ redux authentication/controllers/authentication.js
@@ -0,0 +1,28 @@
+const User = require('../models/user');
+
+exports.signup = function(req, res, next) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // see if a user with the given email exists
+    User.findOne({ email: email }, function(err, existingUser) {
+        if (err) { return next(err); }
+
+        // if a user with email does exist, return an error
+        if (existingUser) {
+            return res.status(422).send({ error: 'Email is in use' });
+        }
+
+        // if a user with email does NOT exist, create and save user record
+        const user = new User({
+            email: email,
+            password: password
+        });
+
+        user.save(function(err) {
+            if (err) { return next(err); }
+
+            return res.json({success: true});
+        })
+    });
+}
--- redux authentication/router.js
+++ redux authentication/router.js
@@ -1,5 +1,5 @@
+const Authentication = require('./controllers/authentication');
+
 module.exports = function(app) {
-    app.get('/', function(req, res, next) {
-        res.send(["TEST1", "TEST2", "TEST3"]);
-    });
+    app.post('/signup', Authentication.signup);
 }
