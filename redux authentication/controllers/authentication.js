f5f95b72d122b8b2a3ad256a122f69792fbc09e1
Rafael Companhoni <companhoni@gmail.com>
2017-07-05 16:58:38 -0300

    Returning JWT on POST /signup

--- redux authentication/controllers/authentication.js
+++ redux authentication/controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
     const email = req.body.email;
@@ -26,7 +33,7 @@ exports.signup = function(req, res, next) {
         user.save(function(err) {
             if (err) { return next(err); }
 
-            return res.json({success: true});
+            return res.json({ token: tokenForUser(user) });
         })
     });
 }
