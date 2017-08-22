b2344d98f27a55fd26b45dafa37aa5dfa15179db
Rafael Companhoni <companhoni@gmail.com>
2017-08-22 17:44:52 -0300

    Mongoose validations

--- Mongoose/crud_with_tests/src/user.js
+++ Mongoose/crud_with_tests/src/user.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: String
+    name: {
+        type: String,
+        validate: {
+            validator: (name) => name.length > 2,
+            message: "Name must be longer than 2 characters."
+        },
+        required: [true, 'Name is required.']
+    },
+    postCount: Number
 });
 
 // 'user': the collection name
--- Mongoose/crud_with_tests/test/update_test.js
+++ Mongoose/crud_with_tests/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({ name: "Joe" });
+        joe = new User({ name: "Joe", postCount: 0 });
         joe.save()
             .then(() => done());
     });
@@ -44,5 +44,15 @@ describe('Updating records', () => {
 
     it('A model class can find a record with an Id and update', (done) => {
         assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex'}), done);
+    });
+
+    // uses $inc operator -- for every 'Joe', increments its 'postCount' and assert that the 'Joe' had its property modified
+    it('A user can have their post count incremented by 1', (done) => {
+        User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then((user) => {
+                assert(user.postCount === 1);
+                done();
+            });
     });
 });
