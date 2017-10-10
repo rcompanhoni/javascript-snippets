8fdecc4e2637cadcbce4a572ec931b1433674f58
Rafael Companhoni <companhoni@gmail.com>
2017-10-10 11:18:55 -0300

    Array left rotation

---
+++ Algorithms/Cracking the Code Interview/Array Left Rotation/array_left_rotation.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var path = require('path');
+
+var file = path.join(__dirname, 'input.txt');
+
+fs.readFile(file, function (err, contents) {
+      if (err) {
+          return console.log(err)
+      }
+
+      main(contents);
+});
+
+function main(contents) {
+    const lines = contents.toString().split(/\r?\n/);
+    const params = lines[0].split(/\s/); 
+
+    const size = params[0];
+    const rotations = params[1];
+    const values = lines[1].split(/\s/);
+    
+    console.log(values.join(" "));
+
+    for (let i = 0; i < rotations; i++) {
+        let element = values.shift();
+        values.push(element);
+    }
+
+    console.log(values.join(" "));
+}
+
+
+
+
