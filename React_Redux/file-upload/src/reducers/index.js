008b55f5720e2ce5943542b2c4d78ca91da02f7e
Rafael Companhoni <companhoni@gmail.com>
2017-11-01 10:58:32 -0200

    Added 'transform-object-rest-spread' to babelrc in order to enable the spread operator notation

--- React_Redux/file-upload/src/reducers/index.js
+++ React_Redux/file-upload/src/reducers/index.js
@@ -3,17 +3,8 @@ import { Reducers as gridReducers } from 'react-redux-grid';
 import ToastReducer from './reducer_toast';
 
 const rootReducer = combineReducers({
+  ...gridReducers,
   alert: ToastReducer,
-
-  bulkAction: gridReducers.bulkAction,
-  dataSource: gridReducers.dataSource,
-  editor: gridReducers.editor,
-  errorHandler: gridReducers.errorHandler,
-  grid: gridReducers.grid,
-  loader: gridReducers.loader,
-  menu: gridReducers.menu,
-  pager: gridReducers.pager,
-  selection: gridReducers.selection,
 });
 
 export default rootReducer;
