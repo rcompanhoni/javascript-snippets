e60e407f2c354c4abc1ae10726a9674be7f2f776
Rafael Companhoni <companhoni@gmail.com>
2017-11-17 19:04:29 -0200

    Dynamically reads files from 'corpus' folder and generates pre-processed files ready for Tree Tagger processing (results placed in folder 'corpus_TT')

--- Node/machine_learning/pre_tree_tagger.js
+++ Node/machine_learning/pre_tree_tagger.js
@@ -2,28 +2,45 @@ var fs = require('fs');
 var path = require('path');
 var cheerio = require('cheerio');
 
-var readFilePath = path.join(__dirname, 'corpus', 'policia.txt');
-var writeFilePath = path.join(__dirname, 'corpus_TT', 'policia_TT.txt');
+var readBasePath = path.join(__dirname, 'corpus');
+var writeBasePath = path.join(__dirname, 'corpus_TT');
 
-fs.readFile(readFilePath, function (err, buffer) {
-      if (err) {
-          return console.log(err)
-      }
+var filePaths = {};
+fs.readdir(readBasePath, (err, files) => {
+    // lê diretório e monta dicionário no formato << path do corpus de leitura, path do corpus TT >>
+    files.forEach(file => {
+        var readFilePath = path.join(readBasePath, file);
+        var writeFilePath = path.join(writeBasePath, file.split('.')[0] + '_TT.txt');
 
-      // lê do arquivo e remove diacríticos (i.e. acentos, cedilha)
-      var contents = buffer.toString();
-      contents = contents.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+        filePaths[readFilePath] = writeFilePath;
+    });
 
-      // parse do HTML 
-      var $ = cheerio.load(contents);
-      var parsedHtml = $('body').text().trim();
+    // pré-processa cada arquivo corpus para corpus TT (i.e. pré-processado para utilização no Tree Tagger)
+    Object.keys(filePaths).forEach((readFilePath) => {
+        fs.readFile(readFilePath, function (err, buffer) {
+              if (err) {
+                  return console.log(err)
+              }
+        
+              // lê do arquivo e remove diacríticos (i.e. acentos, cedilha)
+              var contents = buffer.toString();
+              contents = contents.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+        
+              // parse do HTML 
+              var $ = cheerio.load(contents);
+              var parsedHtml = $('body').text().trim();
+        
+              // salva em arquivo .txt
+              var writeFilePath = filePaths[readFilePath];
+              fs.writeFile(writeFilePath, parsedHtml, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+            
+                console.log('Arquivo criado em', writeFilePath);
+            });    
+        });
+    });
+  });
 
-      // salva em arquivo .txt
-      fs.writeFile(writeFilePath, parsedHtml, function(err) {
-        if(err) {
-            return console.log(err);
-        }
-    
-        console.log("Arquivo pré Tree Tagger gerado.");
-    });    
-});
+
