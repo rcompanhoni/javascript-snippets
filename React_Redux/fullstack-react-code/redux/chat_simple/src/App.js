c51ac1637698d2a27072e9b95eec00d551c9fcb3
Rafael Companhoni <companhoni@gmail.com>
2017-12-20 19:07:09 -0200

    Simple store/reducer/actions for chat app

--- React_Redux/fullstack-react-code/redux/chat_simple/src/App.js
+++ React_Redux/fullstack-react-code/redux/chat_simple/src/App.js
@@ -1,5 +1,5 @@
-function createStore(reducer) {
-  let state = 0;
+function createStore(reducer, initialState) {
+  let state = initialState;
 
   const getState = () => (state);
 
@@ -12,3 +12,57 @@ function createStore(reducer) {
     dispatch,
   };
 }
+
+function reducer(state, action) {
+  if (action.type === 'ADD_MESSAGE') {
+    return {
+      messages: state.messages.concat(action.message),
+    };
+  } else if (action.type === 'DELETE_MESSAGE') {
+    return {
+      messages: [
+        ...state.messages.slice(0, action.index),
+        ...state.messages.slice(action.index + 1, state.messages.length),
+      ],
+    };
+  }
+
+  else {
+    return state;
+  }
+}
+
+const initialState = { messages: [] };
+
+const store = createStore(reducer, initialState);
+
+const addMessageAction1 = {
+  type: 'ADD_MESSAGE',
+  message: 'How does it look, Neil?',
+};
+
+store.dispatch(addMessageAction1);
+const stateV1 = store.getState();
+
+const addMessageAction2 = {
+  type: 'ADD_MESSAGE',
+  message: 'Looking good.',
+};
+
+store.dispatch(addMessageAction2);
+const stateV2 = store.getState();
+console.log('State v1:');
+console.log(stateV1);
+console.log('State v2:');
+console.log(stateV2);
+
+const deleteMessageAction = {
+  type: 'DELETE_MESSAGE',
+  index: 0,
+};
+
+store.dispatch(deleteMessageAction);
+const stateV3 = store.getState();
+
+console.log('State v3:');
+console.log(stateV3);
