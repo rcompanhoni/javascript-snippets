d3a1d68166b612a63399cc066b2e4d951c2d94e1
Rafael Companhoni <companhoni@gmail.com>
2018-07-14 20:20:09 -0300

    GraphQL and Express: complete CRUD with queries and mutations

--- GraphQL/GraphQL_Express_Example/schema/schema.js
+++ GraphQL/GraphQL_Express_Example/schema/schema.js
@@ -4,21 +4,31 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
 } = graphQL;
 
 const CompanyType = new GraphQLObjectType({
     name: 'Company',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
-        description: { type: GraphQLString }
-    }
+        description: { type: GraphQLString },
+        users: { 
+            type: GraphQLList(UserType),
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+                    .then(response => response.data);
+            }
+        }
+    })
 });
 
+// the resolve function is the 'connection' with the node CompanyType 
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt },
@@ -29,10 +39,10 @@ const UserType = new GraphQLObjectType({
                     .then(response => response.data);
             }
         }
-    }
+    })
 });
 
-// provides access to the data graph -- this example reads as 'provide the specified args to get a UserType'
+// provides read access to the data graph
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -44,10 +54,62 @@ const RootQuery = new GraphQLObjectType({
                     .then(response => response.data);
             }
         },
+        company: {
+            type: CompanyType,
+            args: { id: { type: GraphQLString }},
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${args.id}`)
+                    .then(response => response.data);
+            }
+        },
     }
 });
 
+// provides write access to the data graph
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addUser: {
+            type: UserType,
+            args: {
+                firstName: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) },
+                companyId: { type: GraphQLString }
+            },
+            resolve(parentValue, { firstName, age }) {
+                return axios.post('http://localhost:3000/users/', { firstName, age })
+                    .then(response => response.data);
+            }
+        },
+        deleteUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString) }
+            },
+            resolve(parentValue, { id }) {
+                return axios.delete(`http://localhost:3000/users/${id}`)
+                    .then(response => response.data);
+            }
+        },
+        editUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString) },
+                firstName: { type: GraphQLString },
+                age: { type: GraphQLInt },
+                companyId: { type: GraphQLString }
+            },
+            resolve(parentValue, args) {
+                return axios.patch(`http://localhost:3000/users/${args.id}`, args)
+                    .then(response => response.data);
+            }
+        },
+    }
+});
+
+// defines the two root access points for our data graph (query and mutation)
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 });
 
