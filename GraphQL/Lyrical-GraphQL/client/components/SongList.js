6fefbe562ff54c8f319a66123151f9848e1edbe8
Rafael Companhoni <companhoni@gmail.com>
2018-07-16 01:08:05 -0300

    GraphQL: using 'graphql' object (fron 'react-apollo') to fetch the list of songs

---
+++ GraphQL/Lyrical-GraphQL/client/components/SongList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class SongList extends Component {
+  render() {
+    return (
+      <div>
+          SongList
+      </div>
+    );
+  }
+}
+
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
--- GraphQL/Lyrical-GraphQL/client/index.js
+++ GraphQL/Lyrical-GraphQL/client/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import SongList from './components/SongList';
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Lyrical</div>
+      <SongList />
     </ApolloProvider>
   );
 };
