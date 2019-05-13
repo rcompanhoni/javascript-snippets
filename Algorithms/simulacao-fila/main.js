a221ed8fb7da0b26ee437025e457b95c0878f736
Rafael Companhoni <companhoni@gmail.com>
2019-05-13 03:11:26 -0300

    Added 5 queues

--- Algorithms/simulacao-fila/main.js
+++ Algorithms/simulacao-fila/main.js
@@ -1,39 +1,70 @@
 const ARRIVAL = 'CHEGADA';
 const DEPARTURE = 'SAÍDA';
+const TRANSFER = 'TRANSFERENCIA';
+
+function openQueueStats(evt, cityName) {
+  var i, tabcontent, tablinks;
+
+  // get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
 function runSimulation() {
   // parameters
-  const serverQuantity = 1;
-  const capacity = 4;
+  const serverQuantity = 2;
+  const capacity = 3;
   const lambdaMin = 2;
   const lambdaMax = 3;
-  const miMin = 3;
+  const miMin = 2;
   const miMax = 5;
 
   // stop condition
   let eventCount = 1;
-  const eventLimit = 3;
+  const eventLimit = 30;
 
-  const queue2 = new Queue(1, 3, 2, 5, 3, 5);
+  // queue configuration
+  const queue1 = new Queue(1, serverQuantity, capacity, lambdaMin, lambdaMax, miMin, miMax);
+  const queue2 = new Queue(2, 1, 3, 2, 5, 3, 5);
+  const queue3 = new Queue(3, 1, 3, 2, 5, 3, 5);
+  const queue4 = new Queue(4, 1, 3, 2, 5, 3, 5);
+  const queue5 = new Queue(5, 1, 3, 2, 5, 3, 5);
 
-  // queue configuration and initial event
-  const queue1 = new Queue(
-    serverQuantity,
-    capacity,
-    lambdaMin,
-    lambdaMax,
-    miMin,
-    miMax,
-    { queue: queue2, probability: 1 }
-  );
+  // connection config
+  queue1.setConnections({ nextQueue: queue2, probability: 1 });
+  queue2.setConnections({ previousQueue: queue1, nextQueue: queue3 })
+  queue3.setConnections({ previousQueue: queue2, nextQueue: queue4 })
+  queue4.setConnections({ previousQueue: queue3, nextQueue: queue5 })
+  queue5.setConnections({ previousQueue: queue4, })
+  
+  // initial event
   const initialEvent = new Event(ARRIVAL, `${ARRIVAL} 1`, null, 2.5000000000000000);
   queue1.scheduleEvent(initialEvent);
 
   do {
     queue1.runEvent();
     queue2.runEvent();
+    queue3.runEvent();
+    queue4.runEvent();
+    queue5.runEvent();
     eventCount++;
   } while (eventCount <= eventLimit)
 
   queue1.generateResults();
+  queue2.generateResults();
+  queue3.generateResults();
+  queue4.generateResults();
+  queue5.generateResults();
 }
--- Algorithms/simulacao-fila/queue.js
+++ Algorithms/simulacao-fila/queue.js
@@ -1,15 +1,16 @@
 class Queue {
-  constructor(serverQuantity, capacity, lambdaMin, lambdaMax, miMin, miMax, connectsTo) {
+  constructor(position, serverQuantity, capacity, lambdaMin, lambdaMax, miMin, miMax) {
     this.schedule = [];
 
     // parameters
+    this.position = position
+    this.name = `fila${position}`;
     this.serverQuantity = serverQuantity;
     this.capacity = capacity;
     this.lambdaMin = lambdaMin;
     this.lambdaMax = lambdaMax;
     this.miMin = miMin;
     this.miMax = miMax;
-    this.connectsTo = connectsTo;
 
     // state
     this.currentEvent = null;
@@ -24,14 +25,14 @@ class Queue {
 
     this.generateHeader();
 
-    // example from class 1/3
+    // exemplo da aula 1/3
     // this.fakeTimes = [0.1195, 0.3491, 0.9832, 0.7731, 0.8935, 0.2103, 0.0392, 0.1782];
 
-    // example from 2 queue, no rotation class
-    this.fakeTimes = [0.9921, 0.0004, 0.5534, 0.2761, 0.3398, 0.8963, 0.9023, 0.0132, 0.4569, 0.5121, 0.9208, 0.0171, 0.2299, 0.8545, 0.6001, 0.2921];
+    // exemplo da aula de rotação
+    // this.fakeTimes = [0.9921, 0.0004, 0.5534, 0.2761, 0.3398, 0.8963, 0.9023, 0.0132, 0.4569, 0.5121, 0.9208, 0.0171, 0.2299, 0.8545, 0.6001, 0.2921];
   }
 
-  // execute iteration (generates )
+  // execute iteration
   runEvent() {
     if (this.schedule.length == 0) {
       this.scheduleArrival();
@@ -55,13 +56,29 @@ class Queue {
   
         case DEPARTURE:
           this.evaluateProbabilities();
-          this.occupancy--;
-
+          
           // TODO: uses rotation probabilities here
-          if (this.connectsTo) {
+          if (this.connectsTo && this.connectsTo.nextQueue) {
             this.scheduleArrival();
           } else {
             this.scheduleDeparture();
+          }
+          break;
+
+        case TRANSFER:
+          this.evaluateProbabilities();
+
+          this.connectsTo.previousQueue.occupancy--;
+          if (this.connectsTo.previousQueue >= this.connectsTo.previousQueue.capacity) {
+            this.scheduleTrasfer();
+          }
+
+          if (this.occupancy < this.capacity) {
+            this.occupancy++;
+
+            if (this.occupancy <= this.serverQuantity) {
+              this.scheduleDeparture();
+            }
           }
           break;
       }
@@ -96,46 +113,61 @@ class Queue {
 
   // helper: uses linear congruential method -- with variables from 'Numerical Recipes' -- adjusted for values between min and max
   conversion(min, max) {
-    const randomNumber = this.fakeTimes.shift();
+    // const randomNumber = this.fakeTimes.shift();
 
-    // let m = Math.pow(2, 32);
-    // let a = 1664525;
-    // let c = 1013904223;
-    // let z = Date.now();
-    // let randomNumber = (a * z + c) % m;
-    // randomNumber = randomNumber/m;
+    let m = Math.pow(2, 32);
+    let a = 1664525;
+    let c = 1013904223;
+    let z = Date.now();
+    let randomNumber = (a * z + c) % m;
+    randomNumber = randomNumber/m;
 
     return ((max - min) * randomNumber) + min;
   }
 
   // helper: assemble a departure event and adds it to the schedule array
   scheduleDeparture() {
-    const name = `${DEPARTURE} ${this.eventCount + 1}`;
     const raffledTime =  this.conversion(this.miMin, this.miMax);
     const realTime = this.currentEvent.realTime + raffledTime;
-    const departureEvent = new Event(DEPARTURE, name, raffledTime, realTime);
-    this.scheduleEvent(departureEvent);
+    
+    if (this.connectsTo && this.connectsTo.nextQueue) {
+      const name = `${TRANSFER} ${this.position}${this.position + 1}`;
+      const transferEvent = new Event(TRANSFER, name, raffledTime, realTime);
+      this.connectsTo.nextQueue.scheduleEvent(transferEvent)
+    } else {
+      const name = `${DEPARTURE} ${this.position}`;
+      const departureEvent = new Event(DEPARTURE, name, raffledTime, realTime);
+      this.scheduleEvent(departureEvent);
+    }
   }
 
   // helper: assemble an arrival event and adds it to the schedule array
   scheduleArrival() {
-    const name = `${ARRIVAL} ${this.eventCount + 1}`;
+    const name = `${ARRIVAL} ${this.position}`;
     const raffledTime =  this.conversion(this.lambdaMin, this.lambdaMax);
     const realTime = this.currentEvent.realTime + raffledTime;
     const arrivalEvent = new Event(ARRIVAL, name, raffledTime, realTime);
+    this.scheduleEvent(arrivalEvent);
+  }
 
-    if (this.connectsTo) {
-      this.connectsTo.queue.scheduleEvent(arrivalEvent)
-    } else {
-      this.scheduleEvent(arrivalEvent);
-    }
+  // event to transfer from one queue to the next
+  scheduleTransfer() {
+    const name = `${TRANSFER} ${this.position}${this.position + 1}`;
+    const raffledTime =  this.conversion(this.miMin, this.miMax);
+    const realTime = this.currentEvent.realTime + raffledTime;
+    const transferEvent = new Event(TRANSFER, name, raffledTime, realTime);
+    this.connectsTo.nextQueue.scheduleEvent(transferEvent)
+  }
+
+  setConnections(connectsTo) {
+    this.connectsTo = connectsTo;
   }
 
   /************* RESULTS OUTUPUT RELATED *************/
 
-  // create the result's table header
-  generateHeader() {
-    const table = document.getElementById('state-table');
+   // create the result's table header
+   generateHeader() {
+    const table = document.getElementById(`state-table-${this.position}`);
     const newRow = table.insertRow(-1);
 
     this.addCell(newRow, 0, 'evento');
@@ -149,13 +181,17 @@ class Queue {
 
   // update output results table
   updateStateTable() {    
-    const table = document.getElementById('state-table');
+    const table = document.getElementById(`state-table-${this.position}`);
     const newRow = table.insertRow(-1);
     
     this.addCell(newRow, 0, this.currentEvent.name);
     this.addCell(newRow, 1, this.occupancy);
     this.addCell(newRow, 2, this.currentEvent.realTime);
     
+    if (Object.keys(this.probabilities).length > 4) {
+      debugger
+    }
+
     for(let i = 0; i < Object.keys(this.probabilities).length; i++) {
       this.addCell(newRow, i + 3, this.probabilities[i]);
     }
@@ -163,7 +199,7 @@ class Queue {
 
   // populates the results table with the probabilities
   generateResults() {
-    const table = document.getElementById('results-table');
+    const table = document.getElementById(`results-table-${this.position}`);
     const headerRow = table.insertRow(-1);
 
     this.addCell(headerRow, 0, 'Estado da fila');
