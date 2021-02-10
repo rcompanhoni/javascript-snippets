fc687efec5f7fd3ea6890665c84fea065c4d67d1
Rafael Companhoni <companhoni@gmail.com>
2021-02-10 18:22:31 -0300

    Algorithm exercise: reverse integer

--- Algorithms/algo_casts/exercises/reverseint/index.js
+++ Algorithms/algo_casts/exercises/reverseint/index.js
@@ -8,6 +8,9 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let reversed = n.toString().split('').reverse().join('');
+  return Math.sign(n) * parseInt(reversed); // parseInt considers '5-' as '5' so we just need to multiply by the original sign here
+}
 
 module.exports = reverseInt;
