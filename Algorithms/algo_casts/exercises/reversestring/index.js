4eb9db47105b8850854ed21337d5bde4443eba8f
Rafael Companhoni <companhoni@gmail.com>
2021-02-10 17:31:35 -0300

    Algorithm exercise: reversestring

--- Algorithms/algo_casts/exercises/reversestring/index.js
+++ Algorithms/algo_casts/exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// using array built in methods split + reverse + join
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+
+// not using .reverse() -- appends each new char to the start of reversed: a, pa, ppa, eppa, leppa
+function reverse(srt) {
+  let reversed = '';
+
+  for (let character of srt) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
+
+// using reduce() -- same as previous solution
+function reverse(srt) {
+  return srt.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
