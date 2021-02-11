1a04ee18c7ccc929e31d7deff44e611590e19fea
Rafael Companhoni <companhoni@gmail.com>
2021-02-11 11:19:48 -0300

    Algorithm exercise: maxchar

--- Algorithms/algo_casts/exercises/maxchar/index.js
+++ Algorithms/algo_casts/exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // creates a dictionary of frequencies
+  let frequencies = {};
+  str.split('').forEach((char) => {
+    frequencies[char] = frequencies[char] ? frequencies[char] + 1 : 1;
+  });
+
+  // verify which entry in the dictionary has the greatest value
+  // ATTENTION: use for(.. of ..) to iterate strings and arrays and for(.. in .. ) to iterate object key/value keys
+  let mostFrequentValue = -1;
+  let mostFrequentChar = null;
+  for (let char in frequencies) {
+    if (frequencies[char] > mostFrequentValue) {
+      mostFrequentValue = frequencies[char];
+      mostFrequentChar = char;
+    }
+  }
+
+  return mostFrequentChar;
+}
 
 module.exports = maxChar;
