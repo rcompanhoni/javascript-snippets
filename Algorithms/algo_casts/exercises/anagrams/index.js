d31d3ca8a172f7a8640fe2c7254f40f63afb5751
Rafael Companhoni <companhoni@gmail.com>
2021-02-18 20:37:50 -0300

    Algorithm exercise: capitalize

--- Algorithms/algo_casts/exercises/anagrams/index.js
+++ Algorithms/algo_casts/exercises/anagrams/index.js
@@ -8,6 +8,48 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+/* SOLUTION 1 - USING A CHAR MAP -- this solution makes 3 iterations */
+
+function getCharMap(inputString) {
+  // filter out punctuation and converts to lower case
+  const parsedString = inputString.replace(/[^\w]/g, '').toLowerCase();
+
+  // assembles the char map
+  return parsedString.split('').reduce((acc, char) => {
+    acc[char] = acc[char] + 1 || 1;
+    return acc;
+  }, {});
+}
+
+function anagrams(stringA, stringB) {
+  // get dictionary of char frequencies
+  const charMapA = getCharMap(stringA);
+  const charMapB = getCharMap(stringB);
+
+  // ensure one is not a substring of another (e.g. 'hell' and 'hello')
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+    return false;
+  }
+
+  // ensure both dictionaries have the same frequencies
+  for (let charKey in charMapA) {
+    if (charMapA[charKey] !== charMapB[charKey]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/* SOLUTION 2 - USING THE METHOD SORT -- this solution makes np iterations */
+
+// filter out punctuation, converts to lower case, split to an array, SORT THE ARRAY and join it back to a simple string
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB); // just make a simple comparison between the parsed (and sorted) strings
+}
 
 module.exports = anagrams;
--- Algorithms/algo_casts/exercises/capitalize/index.js
+++ Algorithms/algo_casts/exercises/capitalize/index.js
@@ -7,6 +7,32 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// split the words and, for each word capitalize the first letter and then join it to the remaining of the word
+function capitalize(str) {
+  const capitalized = [];
+
+  for (let word of str.split(' ')) {
+    const firstLetter = word[0].toUpperCase();
+    const remaining = word.slice(1);
+    capitalized.push(`${firstLetter}${remaining}`);
+  }
+
+  return capitalized.join(' ');
+}
+
+// split each element in the string and iterate through it -- if the previous element is a space then capitalize it add it to the result
+function capitalize(str) {
+  let result = str[0].toUpperCase(); // the overall strategy doesn't work for the first character so we initialize the result array with the first letter already capitalized
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
 
 module.exports = capitalize;
