d4f0f7f45688d3636c056a36c9dd140f8859a437
Rafael Companhoni <companhoni@gmail.com>
2021-03-15 23:19:31 -0300

    Algorithm exercise: steps

--- Algorithms/algo_casts/exercises/steps/index.js
+++ Algorithms/algo_casts/exercises/steps/index.js
@@ -17,6 +17,43 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+
+    console.log(stair);
+  }
+}
+
+// recursive version
+function steps(n, row = 0, stair = '') {
+  // base case, when it gets to the last row we already printed all rows so just finish
+  if (n === row) {
+    return;
+  }
+
+  // at the end of a row, print the stair
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  // fill the stair if not in the end yet
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  return steps(n, row, stair);
+}
 
 module.exports = steps;
